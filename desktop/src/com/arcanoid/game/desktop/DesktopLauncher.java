76964c573598bd2173e17c85d8515c9c008d95c6
NikitaPlombeer <francuzov72@gmail.com>
2015-04-19 10:56:17 +0300

    Initial commit

---
+++ desktop/src/com/arcanoid/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arcanoid.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arcanoid.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
